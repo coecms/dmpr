48305bcb213764e1bcdb60b350523a9faee9e286
Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
2017-08-22 16:56:31 +1000

    Omitted mom test script. Fixed

---
+++ test/mom/test_mom.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+"""
+Copyright 2017 ARC Centre of Excellence for Climate Systems Science
+
+author: Scott Wales <scott.wales@unimelb.edu.au>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from dmpr.mom.model import *
+from dmpr.model import identify_model
+
+import os
+import netCDF4
+import pytest
+
+sample = os.path.join(os.path.dirname(os.path.realpath(__file__)),'sample')
+
+def test_identify_model():
+    assert isinstance(identify_model(sample), MOM)
+    
+@pytest.fixture(scope='module')
+def archivedir(tmpdir_factory):
+    return str(tmpdir_factory.mktemp('um'))
+
+@pytest.fixture(scope='module')
+def sample_out(archivedir):
+    """
+    Returns a processed output file
+    """
+    model = MOM()
+    model.archivedir = archivedir
+
+    infile = os.path.join(sample, 'ocean_month.nc')
+    outfile = model.post(infile)
+    return outfile
+
+def test_metadata(sample_out):
+    with netCDF4.Dataset(sample_out) as d:
+        # There is a history present
+        assert d.getncattr('history') is not None
+
+def test_cfcheck(sample_out, cfchecker):
+    # Output is CF compliant
+    assert cfchecker.checker(sample_out) == 0
+
+@pytest.fixture(scope='module')
+def sample_dmp(archivedir, dmp):
+    """
+    Returns a processed output file
+    """
+    model = MOM()
+    model.archivedir = archivedir
+    model.dmp = dmp
+
+    infile = os.path.join(sample, 'ocean_month.nc')
+    outfile = model.post(infile, 'ocean_month.dmp.nc')
+    return outfile
+
+def test_metadata_dmp(sample_dmp):
+    with netCDF4.Dataset(sample_dmp) as d:
+        assert d.getncattr('data_management_plan') is not None
+
+def test_cfcheck_dmp(sample_dmp, cfchecker):
+    # Output is CF compliant
+    assert cfchecker.checker(sample_dmp) == 0
